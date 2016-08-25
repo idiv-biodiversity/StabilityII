2da063136742641bb6f201daec718a13d9d5cf01
Dylan Craven <dylancraven98403@gmail.com>
2016-08-25 15:06:54 +0200

    finish path coefficients, Resistance

--- Rst_PathCoefficients.R
+++ Rst_PathCoefficients.R
@@ -275,6 +275,9 @@ exdry1$PD_met<-"ePSE"
 exdry1$FD_met<-"FRic"
 exdry1$X<-NULL
 
+
+# include path coefficients for FRic and ePSE from different models for visualisation 
+
 wet<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_WET_epse_fric_sem_coefs_V1.csv",sep=",",header=TRUE)
 wet1<-filter(wet,response=="lg2Rst12")
 wet1$PD_met<-"ePSE"
@@ -349,3 +352,194 @@ png(filename="/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level
 p4
 
 dev.off()
+
+
+###################
+### Model 5 #######
+###################
+
+
+dry<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_DRY_emntd_fric_sem_coefs.csv",sep=",",header=TRUE)
+dry1<-filter(dry,response=="lg2Rst12")
+dry1$PD_met<-"eMNTD"
+dry1$FD_met<-"FRic"
+dry1$X<-NULL
+
+exdry<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_EXDRY_emntd_fric_sem_coefs.csv",sep=",",header=TRUE)
+exdry1<-filter(exdry,response=="lg2Rst12")
+exdry1$PD_met<-"eMNTD"
+exdry1$FD_met<-"FRic"
+exdry1$X<-NULL
+
+#  use path coefficients from separate models for visualization purposes
+wet<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_WET_emntd_fric_sem_coefs_V1.csv",sep=",",header=TRUE)
+wet1<-filter(wet,response=="lg2Rst12")
+wet1$PD_met<-"eMNTD"
+wet1$FD_met<-"FRic"
+wet1$X<-NULL
+
+
+wet2<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_WET_emntd_fric_sem_coefs_V2.csv",sep=",",header=TRUE)
+wet22<-filter(wet2,response=="lg2Rst12")
+wet22$PD_met<-"eMNTD"
+wet22$FD_met<-"FRic"
+wet22$X<-NULL
+
+wet22<-wet22[3,]
+
+wet11<-rbind.data.frame(wet1,wet22)
+
+exwet<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_EXWET_emntd_fric_sem_coefs.csv",sep=",",header=TRUE)
+exwet1<-filter(exwet,response=="lg2Rst12")
+exwet1$PD_met<-"eMNTD"
+exwet1$FD_met<-"FRic"
+exwet1$X<-NULL
+
+paths5<-rbind.data.frame(dry1,exdry1, wet11,exwet1)
+
+paths5$lowCI<-paths5$estimate-(paths5$std.error*1.96)
+paths5$highCI<-paths5$estimate+(paths5$std.error*1.96)
+
+paths5$predictor<-as.character(paths5$predictor)
+paths5$predictor<-ifelse(paths5$predictor=="lg2SppN","Species Richness",paths5$predictor)
+paths5$predictor<-ifelse(paths5$predictor=="Yn_PCAcwm4trts","Fast-Slow (CWM)",paths5$predictor)
+paths5$predictor<-ifelse(paths5$predictor=="Yn_eMNTD","Phylogenetic Diversity",paths5$predictor)
+paths5$predictor<-ifelse(paths5$predictor=="Yn_FRic4","Functional Diversity",paths5$predictor)
+#paths5$predictor<-ifelse(paths5$predictor=="Yn_FDis4","Functional Diversity",paths5$predictor)
+
+paths5$predictor<-as.factor(paths5$predictor)
+
+paths5$PathSig<-ifelse(paths5$p.value<0.05,1,0)
+paths5$PathSig<-as.factor(paths5$PathSig)
+
+##Figure
+
+p5<-ggplot(data=paths5,aes(x=Climate_Bin,y=estimate,group=predictor,colour=PathSig))+
+  geom_hline(yintercept=0,linetype=3,color="gray40")+
+  geom_point(size=2,shape=19)+
+  geom_errorbar(width=0.2,aes(ymin=lowCI,ymax=highCI))+
+  scale_y_continuous(limits=c(-0.45,.5),breaks=c(-0.4,-.3,-.2,0,-.1,0,.1,.2,.3,0.4))+
+  
+  scale_colour_manual(name="",values = c("1"= "#de2d26","0" ="#3182bd"))+
+  
+  labs(x = "", y = "Standardized path coefficient (95 % CI)") + ggtitle("Resistance (SPEI12)")+
+  
+  facet_wrap(~predictor,ncol=2,nrow=2)+
+  panel_border(colour = "black", size = 0.5, linetype = 1,
+               remove = FALSE)+
+  theme(plot.title=element_text(size=8,face="bold"),
+        axis.text.x=element_text(size=8,face="bold"),
+        axis.text.y=element_text(size=6),
+        axis.title.y=element_text(size=8,face="bold"),
+        strip.text.x=element_text(size=6,face="bold"),
+        legend.position="none",
+        panel.background =element_rect(fill="transparent",colour="black"),panel.grid.minor=element_blank())
+
+png(filename="/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/PathCoefficients_Rst_FRicEMNTD.png", 
+    type="cairo",
+    units="in", 
+    width=5 ,
+    height=5, 
+    pointsize=2, 
+    res=200)
+
+p5
+
+dev.off()
+
+
+##################
+# Model 6  #######
+##################
+
+dry<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_DRY_empd_fric_sem_coefs.csv",sep=",",header=TRUE)
+dry1<-filter(dry,response=="lg2Rst12")
+dry1$PD_met<-"eMPD"
+dry1$FD_met<-"FRic"
+dry1$X<-NULL
+
+exdry<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_EXDRY_empd_fric_sem_coefs.csv",sep=",",header=TRUE)
+exdry1<-filter(exdry,response=="lg2Rst12")
+exdry1$PD_met<-"eMPD"
+exdry1$FD_met<-"FRic"
+exdry1$X<-NULL
+
+
+
+#  use path coefficients from separate models for visualization purposes
+wet<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_WET_empd_fric_sem_coefs_V1.csv",sep=",",header=TRUE)
+wet1<-filter(wet,response=="lg2Rst12")
+wet1$PD_met<-"eMPD"
+wet1$FD_met<-"FRic"
+wet1$X<-NULL
+
+
+wet2<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_WET_empd_fric_sem_coefs_V2.csv",sep=",",header=TRUE)
+wet22<-filter(wet2,response=="lg2Rst12")
+wet22$PD_met<-"eMPD"
+wet22$FD_met<-"FRic"
+wet22$X<-NULL
+
+wet22<-wet22[3,]
+
+wet11<-rbind.data.frame(wet1,wet22)
+
+exwet<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Rst_EXWET_empd_fric_sem_coefs.csv",sep=",",header=TRUE)
+exwet1<-filter(exwet,response=="lg2Rst12")
+exwet1$PD_met<-"eMPD"
+exwet1$FD_met<-"FRic"
+exwet1$X<-NULL
+
+paths6<-rbind.data.frame(dry1,exdry1, wet11,exwet1)
+
+paths6$lowCI<-paths6$estimate-(paths6$std.error*1.96)
+paths6$highCI<-paths6$estimate+(paths6$std.error*1.96)
+
+paths6$predictor<-as.character(paths6$predictor)
+paths6$predictor<-ifelse(paths6$predictor=="lg2SppN","Species Richness",paths6$predictor)
+paths6$predictor<-ifelse(paths6$predictor=="Yn_PCAcwm4trts","Fast-Slow (CWM)",paths6$predictor)
+paths6$predictor<-ifelse(paths6$predictor=="Yn_eMPD","Phylogenetic Diversity",paths6$predictor)
+paths6$predictor<-ifelse(paths6$predictor=="Yn_FRic4","Functional Diversity",paths6$predictor)
+#paths5$predictor<-ifelse(paths5$predictor=="Yn_FDis4","Functional Diversity",paths5$predictor)
+
+paths6$predictor<-as.factor(paths6$predictor)
+
+paths6$PathSig<-ifelse(paths6$p.value<0.05,1,0)
+paths6$PathSig<-as.factor(paths6$PathSig)
+
+##Figure
+
+p6<-ggplot(data=paths5,aes(x=Climate_Bin,y=estimate,group=predictor,colour=PathSig))+
+  geom_hline(yintercept=0,linetype=3,color="gray40")+
+  geom_point(size=2,shape=19)+
+  geom_errorbar(width=0.2,aes(ymin=lowCI,ymax=highCI))+
+  scale_y_continuous(limits=c(-0.45,.5),breaks=c(-0.4,-.3,-.2,0,-.1,0,.1,.2,.3,0.4))+
+  
+  scale_colour_manual(name="",values = c("1"= "#de2d26","0" ="#3182bd"))+
+  
+  labs(x = "", y = "Standardized path coefficient (95 % CI)") + ggtitle("Resistance (SPEI12)")+
+  
+  facet_wrap(~predictor,ncol=2,nrow=2)+
+  panel_border(colour = "black", size = 0.5, linetype = 1,
+               remove = FALSE)+
+  theme(plot.title=element_text(size=8,face="bold"),
+        axis.text.x=element_text(size=8,face="bold"),
+        axis.text.y=element_text(size=6),
+        axis.title.y=element_text(size=8,face="bold"),
+        strip.text.x=element_text(size=6,face="bold"),
+        legend.position="none",
+        panel.background =element_rect(fill="transparent",colour="black"),panel.grid.minor=element_blank())
+
+png(filename="/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/PathCoefficients_Rst_FRicEMPD.png", 
+    type="cairo",
+    units="in", 
+    width=5 ,
+    height=5, 
+    pointsize=2, 
+    res=200)
+
+p6
+
+dev.off()
+
+
