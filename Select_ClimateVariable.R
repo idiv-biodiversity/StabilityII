0248de09d28f073e4a316bb6c9664976b0a19640
Dylan Craven <dylancraven98403@gmail.com>
2016-10-12 17:24:06 +0200

    Choose climate variable to include:CV_Precip

---
+++ Select_ClimateVariable.R
@@ -0,0 +1,54 @@
+rm(list=ls()) 
+
+library(dplyr)
+
+library(ggplot2)
+
+#library(lars)
+
+# Data
+stab<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Stab_Stability_FD_PD_CWM_PlotYearAverages_V.csv",sep=",",header=T)
+
+stab<-filter(stab,Site!="BIODEPTH_GR")  # should get rid of site where we didn't have good trait coverage
+
+stab_4<-select(stab,Site,UniqueID,SppN,eMPD,eMNTD,ePSE,FDis4,FRic4,PCAdim1_4trts,Plot_TempStab,Plot_Asynchrony,meanPrecip,annualTemp,meanPET,CV_Temp,CV_Precip)
+
+stab_4$Plot_Asynchrony<-ifelse(stab_4$SppN==1 & is.na(stab_4$Plot_Asynchrony)==TRUE,1,stab_4$Plot_Asynchrony) # for monocultures, we assume that a species
+#is perfectly synchronized with itself
+
+
+stab_4$SppN<-as.numeric(stab_4$SppN)
+
+stab_4$TS_lg2<-log(stab_4$Plot_TempStab,base=2)
+
+stab_4$lg2SppN <- log(stab_4$SppN,2)
+
+
+stab_444<-stab_4[!is.na(stab_4$Plot_Asynchrony),]  # no NAs for Plot Asynchrony
+stab_444<-stab_444[!is.na(stab_444$FRic4),]  # no NAs for Plot Asynchrony
+
+cc<-lmeControl(opt="optim")
+
+
+stab_clim<-summarize(group_by(stab_444,Site),meanTS=mean(Plot_TempStab),meanPrecip=mean(meanPrecip),meanTemp=mean(annualTemp),meanPET=mean(meanPET),
+                    CV_Temp=mean(CV_Temp),CV_Precip=mean(CV_Precip))
+
+stab_clim$TS_lg2<-log(stab_clim$meanTS,base=2)
+
+############################
+## div + climate ###########
+############################
+
+Cand.set <- list( )
+Cand.set[[1]]<-lmer(TS_lg2~meanPrecip+(1+lg2SppN|Site),data=stab_444)
+Cand.set[[2]]<-lmer(TS_lg2~annualTemp+(1+lg2SppN|Site),data=stab_444)
+Cand.set[[3]]<-lmer(TS_lg2~meanPET+(1+lg2SppN|Site),data=stab_444)
+Cand.set[[4]]<-lmer(TS_lg2~CV_Temp+(1+lg2SppN|Site),data=stab_444)
+Cand.set[[5]]<-lmer(TS_lg2~CV_Precip+(1+lg2SppN|Site),data=stab_444)
+
+
+Modnames <- paste("Mod", 1:length(Cand.set), sep = " ")
+res.table <- aictab(cand.set = Cand.set, modnames = Modnames,second.ord = T)
+res.table
+
+# select CV_Temp :  delta AICc = 1.13
