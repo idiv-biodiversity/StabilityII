3ad7385931bab446e5255923f43a5a1994cf87c4
Dylan Craven <dylancraven98403@gmail.com>
2016-10-24 13:57:39 +0200

    Calculate effect sizes for diversity-related measures

---
+++ Div_Corr_EffSizes.R
@@ -0,0 +1,152 @@
+require(dplyr)
+require(psych)
+require(dcast2)
+require(metafor)
+require(corrplot)
+
+is.nan.data.frame <- function(x)
+  do.call(cbind, lapply(x, is.nan))
+
+
+# Data
+stab<-read.delim("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Stab_Stability_FD_PD_CWM_PlotYearAverages_V.csv",sep=",",header=T)
+
+stab<-filter(stab,Site!="BIODEPTH_GR")  # should get rid of site where we didn't have good trait coverage
+
+stab_4<-select(stab,Site,UniqueID,SppN,eMPD,eMNTD,ePSE,FDis4,FRic4,PCAdim1_4trts,Plot_TempStab,Plot_Asynchrony,CV_Temp)
+
+stab_4$Plot_Asynchrony<-ifelse(stab_4$SppN==1 & is.na(stab_4$Plot_Asynchrony)==TRUE,1,stab_4$Plot_Asynchrony) # for monocultures, we assume that a species
+#is perfectly synchronized with itself
+
+stab_4$SppN<-as.numeric(stab_4$SppN)
+
+stab_4$TS_lg2<-log(stab_4$Plot_TempStab,base=2)
+
+stab_4$lg2SppN <- log(stab_4$SppN,2)
+
+#stab_44<-filter(stab_4,SppN>1) eliminate monocultures
+
+stab_44<-stab_4[!is.na(stab_4$Plot_Asynchrony),]  # no NAs for Plot Asynchrony
+
+stab_444<-stab_4[!is.na(stab_4$Plot_Asynchrony),]  # no NAs for Plot Asynchrony
+stab_444<-stab_444[!is.na(stab_444$FRic4),]  # no NAs for Plot Asynchrony
+
+#################################
+# calculate correlations among  #
+# all predictors of TS ##########
+#################################
+
+stab_corr<-select(stab_444,Site,SppN, eMPD, eMNTD,ePSE, FDis4, FRic4,PCAdim1_4trts,Plot_Asynchrony)
+
+n<-length(unique(stab_corr$Site))
+
+outt=c();
+
+for(i in 1:n){
+  
+  test=subset(stab_corr, stab_corr$Site==(unique(stab_corr$Site))[i]) 
+  
+  Site<-as.character(unique(test$Site))
+  Plot_n<-dim(test)[1]
+  test<-select(test,-Site)
+  
+  p_out<-corr.test(test,method="pearson")
+  p_outt<-as.matrix(p_out$r)
+  p_outt<-data.frame(Var1=rownames(p_outt)[row(p_outt)[upper.tri(p_outt)]], 
+                     Var2=colnames(p_outt)[col(p_outt)[upper.tri(p_outt)]], 
+             corr=p_outt[upper.tri(p_outt)])
+  
+  p_outt$Site<-Site
+  p_outt$Plot_n<-Plot_n
+
+    outt[[i]]<-rbind.data.frame(p_outt)
+  
+}
+
+jjj<-do.call(rbind,outt)  
+jjj<-data.frame(jjj)
+
+jjj<-na.omit(jjj)
+
+
+#Calculate effect size (raw correlation coefficient)
+
+effect_size<-select(jjj,Site, Plot_n,Var1,Var2, corr)
+effect_size<-escalc(measure="COR",ri=corr,ni=Plot_n,data=effect_size)
+
+######### 
+
+effect_size$Combn<-paste(effect_size$Var1,effect_size$Var2,sep="_")
+
+n<-length(unique(effect_size$Combn))
+
+outt=c();
+
+for(i in 1:n){
+  
+  test=subset(effect_size, effect_size$Combn==(unique(effect_size$Combn))[i]) 
+
+  Combn<-as.character(unique(test$Combn))
+  Var1<-as.character(unique(test$Var1))
+  Var2<-as.character(unique(test$Var2))
+  
+    Mod1<-rma.mv(yi,vi,random=~1|Site,data=test)
+
+  outt_p<-cbind.data.frame(Var1, Var2,Combn,Mod1$b,Mod1$ci.lb,Mod1$ci.ub)
+  outt[[i]]<-rbind.data.frame(outt_p)
+  
+}
+
+jjj<-do.call(rbind,outt)  
+jjj<-data.frame(jjj)
+
+colnames(jjj)[4]<-"r"
+colnames(jjj)[5]<-"lower95"
+colnames(jjj)[6]<-"upper95"
+
+jjj<-arrange(jjj,Var1,Var2)
+
+corr_mat<-dcast(jjj,Var1~Var2,value.var="r",mean)
+rownames(corr_mat)<-corr_mat$Var1
+corr_mat$Var1<-as.character(corr_mat$Var1)
+
+corr_mat$Var1<-ifelse(corr_mat$Var1=="PCAdim1_4trts","Fast-slow",corr_mat$Var1)
+
+corr_mat<-select(corr_mat,-Plot_Asynchrony)
+
+colnames(corr_mat)[7]<-"Fast-slow"
+
+corr_mat$Var1<-NULL
+
+corr_mat[is.nan(corr_mat)] <- 0
+corr_mat<-as.matrix(corr_mat)
+
+
+col<- colorRampPalette(c("red", "white", "blue"))(256)
+
+col2<-plasma(256)
+
+
+##################
+
+png(filename="/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Div_Corr.png", 
+    type="cairo",
+    units="in", 
+    width=6, 
+    height=6 , 
+    pointsize=2, 
+    res=200)
+
+
+corrplot(corr_mat, method="ellipse",type="upper",col=col,is.corr=TRUE,diag=TRUE,bg="white",tl.pos=TRUE,tl.cex=6,tl.col="black",tl.srt=0,cl.cex=6)
+
+dev.off()
+
+cairo_ps("/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/Div_Corr.eps",
+         family="sans",
+         height=6,width=6,
+         bg="white")
+
+corrplot(corr_mat, method="ellipse",type="upper",col=col,is.corr=TRUE,diag=TRUE,bg="white",tl.pos=TRUE,tl.col="black",tl.srt=0)
+
+dev.off()
--- piecewiseSEM_Coefficients_Figures_nosynchrony.R
+++ piecewiseSEM_Coefficients_Figures_nosynchrony.R
@@ -14,19 +14,171 @@ nosync1<-nosync1 %>%
   separate(ModClass, c("FDclass", "PDclass"), "_")
 nosync1$FDclass2<-"FD"
 
-sync1$predictor<-as.character(sync1$predictor)
-sync1$predictor<-ifelse(sync1$predictor=="PCAdim1_4trts","Fast-Slow Axis",sync1$predictor)
-sync1$predictor<-ifelse(sync1$predictor=="lg2SppN","Species Richness",sync1$predictor)
-sync1$predictor<-ifelse(sync1$predictor=="FDis4" |sync1$predictor=="FRic4","FD",sync1$predictor)
-sync1$predictor<-ifelse(sync1$predictor=="eMNTD" |sync1$predictor=="ePSE"|sync1$predictor=="eMPD","PD",sync1$predictor)
+nosync1$predictor<-as.character(nosync1$predictor)
+nosync1$predictor<-ifelse(nosync1$predictor=="PCAdim1_4trts","Fast-Slow Axis",nosync1$predictor)
+nosync1$predictor<-ifelse(nosync1$predictor=="lg2SppN","Species Richness",nosync1$predictor)
+nosync1$predictor<-ifelse(nosync1$predictor=="FDis4" |nosync1$predictor=="FRic4","FD",nosync1$predictor)
+nosync1$predictor<-ifelse(nosync1$predictor=="eMNTD" |nosync1$predictor=="ePSE"|nosync1$predictor=="eMPD","PD",nosync1$predictor)
 
-sync1$predictor<-as.factor(sync1$predictor)
+nosync1$predictor<-as.factor(nosync1$predictor)
 
-sync1$response<-"Synchrony"
+nosync1$response<-"Synchrony"
 
 
-sync1$lowCI<-sync1$estimate-(sync1$std.error*1.96)
-sync1$highCI<-sync1$estimate+(sync1$std.error*1.96)
+nosync1$lowCI<-nosync1$estimate-(nosync1$std.error*1.96)
+nosync1$highCI<-nosync1$estimate+(nosync1$std.error*1.96)
 
-sync1$PathSig<-ifelse(sync1$p.value<0.05,1,0)
-sync1$PathSig<-as.factor(sync1$PathSig)
+nosync1$PathSig<-ifelse(nosync1$p.value<0.05,1,0)
+nosync1$PathSig<-as.factor(nosync1$PathSig)
+
+
+[[stop here]]
+########
+##SppN #
+########
+
+sppn2<-filter(sync2,predictor=="Species Richness")
+
+p11<-ggplot(data=sppn2,aes(x=PDclass,y=estimate,group=FDclass,colour=PathSig))+
+  geom_hline(yintercept=0,linetype=3,color="gray40")+
+  geom_point(size=2,shape=19)+
+  geom_errorbar(width=0.2,aes(ymin=lowCI,ymax=highCI))+
+  scale_y_continuous(limits=c(-1,1))+
+  
+  scale_colour_manual(name="",values = c("1"= "#de2d26","0" ="#3182bd"))+
+  
+  labs(x = "", y = expression(bold(paste("Ecosystem stability ( ",mu," / ",sigma," )")))) + ggtitle("Plant species richness")+
+  
+  facet_grid(~FDclass)+
+  panel_border(colour = "black", size = 0.5, linetype = 1,
+               remove = FALSE)+
+  theme(plot.title=element_text(size=8,face="bold"),
+        axis.text.x=element_blank(),
+        axis.text.y=element_text(size=6),
+        axis.title.y=element_text(size=8,face="bold"),
+        strip.text.x=element_text(size=6,face="bold"),
+        legend.position="none",
+        panel.background =element_rect(fill="transparent",colour="black"),panel.grid.minor=element_blank())
+
+fss2<-filter(sync2,predictor=="Fast-Slow Axis")
+
+p22<-ggplot(data=fss2,aes(x=PDclass,y=estimate,group=FDclass,colour=PathSig))+
+  geom_hline(yintercept=0,linetype=3,color="gray40")+
+  geom_point(size=2,shape=19)+
+  geom_errorbar(width=0.2,aes(ymin=lowCI,ymax=highCI))+
+  scale_y_continuous(limits=c(-1,1))+
+  
+  scale_colour_manual(name="",values = c("1"= "#de2d26","0" ="#3182bd"))+
+  
+  labs(x = "", y=expression(bold(paste("Ecosystem stability ( ",mu," / ",sigma," )"))))+ ggtitle("Fast-slow axis")+
+  
+  facet_grid(~FDclass)+
+  panel_border(colour = "black", size = 0.5, linetype = 1,
+               remove = FALSE)+
+  theme(plot.title=element_text(size=8,face="bold"),
+        axis.text.x=element_blank(),
+        axis.text.y=element_text(size=6),
+        axis.title.y=element_blank(),
+        strip.text.x=element_text(size=6,face="bold"),
+        legend.position="none",
+        panel.background =element_rect(fill="transparent",colour="black"),panel.grid.minor=element_blank())
+
+
+fdd2<-filter(sync2,predictor=="CV_Precip")
+
+
+p33<-ggplot(data=fdd2,aes(x=PDclass,y=estimate,group=FDclass,colour=PathSig))+
+  geom_hline(yintercept=0,linetype=3,color="gray40")+
+  geom_point(size=2,shape=19)+
+  geom_errorbar(width=0.2,aes(ymin=lowCI,ymax=highCI))+
+  scale_y_continuous(limits=c(-1,1))+
+  
+  scale_colour_manual(name="",values = c("1"= "#de2d26","0" ="#3182bd"))+
+  
+  labs(x = "", y=expression(bold(paste("Ecosystem stability ( ",mu," / ",sigma," )"))))+ ggtitle("Precipitation seasonality")+
+  
+  facet_grid(~FDclass)+
+  panel_border(colour = "black", size = 0.5, linetype = 1,
+               remove = FALSE)+
+  theme(plot.title=element_text(size=8,face="bold"),
+        axis.text.x=element_text(size=6),
+        axis.text.y=element_text(size=6),
+        axis.title.y=element_text(size=8,face="bold"),
+        strip.text.x=element_text(size=6,face="bold"),
+        legend.position="none",
+        panel.background =element_rect(fill="transparent",colour="black"),panel.grid.minor=element_blank())
+
+
+pdd2<-filter(sync2,predictor=="PD")
+
+empty<-filter(sync2,predictor=="Fast-Slow Axis")
+empty$estimate<-0
+empty$predictor<-"PD"
+empty$lowCI<-0
+empty$highCI<-0
+empty$PathSig<-0
+empty<-filter(empty,Model!="FDis_eMNTD")
+empty<-filter(empty,Model!="FRic_eMNTD")
+pdd2<-rbind.data.frame(pdd2,empty)
+
+p44<-ggplot(data=pdd2,aes(x=PDclass,y=estimate,group=FDclass,colour=PathSig))+
+  geom_hline(yintercept=0,linetype=3,color="gray40")+
+  geom_point(size=2,shape=19)+
+  geom_errorbar(width=0.2,aes(ymin=lowCI,ymax=highCI))+
+  scale_y_continuous(limits=c(-1,1))+
+  
+  scale_colour_manual(name="",values = c("1"= "#de2d26","0" ="white"))+
+  
+  labs(x = "", y=expression(bold(paste("Ecosystem stability ( ",mu," / ",sigma," )")))) + ggtitle("Phylogenetic diversity")+
+  
+  facet_grid(~FDclass)+
+  panel_border(colour = "black", size = 0.5, linetype = 1,
+               remove = FALSE)+
+  theme(plot.title=element_text(size=8,face="bold"),
+        axis.text.x=element_text(size=6),
+        axis.text.y=element_text(size=6),
+        axis.title.y=element_blank(),
+        strip.text.x=element_text(size=6,face="bold"),
+        legend.position="none",
+        panel.background =element_rect(fill="transparent",colour="black"),panel.grid.minor=element_blank())
+
+fdd5<-filter(sync2,predictor=="eta")
+
+
+p55<-ggplot(data=fdd5,aes(x=PDclass,y=estimate,group=FDclass,colour=PathSig))+
+  geom_hline(yintercept=0,linetype=3,color="gray40")+
+  geom_point(size=2,shape=19)+
+  geom_errorbar(width=0.2,aes(ymin=lowCI,ymax=highCI))+
+  scale_y_continuous(limits=c(-1,1))+
+  
+  scale_colour_manual(name="",values = c("1"= "#de2d26","0" ="#3182bd"))+
+  
+  labs(x = "", y=expression(bold(paste("Ecosystem stability ( ",mu," / ",sigma," )"))))+ ggtitle(expression(eta))+
+  
+  facet_grid(~FDclass)+
+  panel_border(colour = "black", size = 0.5, linetype = 1,
+               remove = FALSE)+
+  theme(plot.title=element_text(size=8,face="bold"),
+        axis.text.x=element_text(size=6),
+        axis.text.y=element_text(size=6),
+        axis.title.y=element_blank(),
+        strip.text.x=element_text(size=6,face="bold"),
+        legend.position="none",
+        panel.background =element_rect(fill="transparent",colour="black"),panel.grid.minor=element_blank())
+
+
+#########
+
+png(filename="/home/dylan/Dropbox/leipzigPhyTrt/StabilityII_data/Community_Level/PathCoefficients_TS_nosync_NEW.png", 
+    type="cairo",
+    units="in", 
+    width=8, 
+    height=8, 
+    pointsize=2, 
+    res=200)
+
+plot_grid(p11,p22,p44,p33,p55,ncol=3,nrow=2,labels=c("(a)","(b)","(c)","(d)","(e)"),label_size=7,align="hv")
+
+
+dev.off()
+
