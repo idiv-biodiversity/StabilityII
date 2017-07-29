2ea0c01d63c9ab5b99a138ab7657736223144727
dylancraven <dylancraven98403@gmail.com>
2017-07-29 12:59:15 +0200

    Asynchrony measures following 'codyn'

--- codyn_addLoreauSynchrony.R
+++ codyn_addLoreauSynchrony.R
@@ -20,110 +20,97 @@ all<-all[order(all$Study,all$Site,all$BlockUnique,all$PlotUnique,all$SppN,all$Sp
 all$UniqueID<-paste(all$Study,"_",all$Site,"_",all$Block,"_",all$Plot)
 all$UniqueID<-str_replace_all(all$UniqueID," ","")
 
+##############
+# set up data#
+##############
+
+
 # quality check, species numbers per site
 
 spp_site<-data.frame(tapply(all$SppCode6,all$Site,length_unique))
 spp_site$Site<-rownames(spp_site)
 colnames(spp_site)[1]<-"Spp_Num"
 
+## plots for which asynchrony should be calculated
 
-########################
-# use codyn ############
-########################
+stab<-read.delim("/homes/dc78cahe/Dropbox (iDiv)/Research_projects/leipzigPhyTrt/StabilityII_data/Community_Level/Stab_Stability_FD_PD_CWM_PlotYearAverages_V.csv",sep=",",header=T)
 
-all_sync<-select(all, Study, Site, UniqueID, SppN, Year, SppCode6, Spp_Biomass)
+stab<-filter(stab,Site!="BIODEPTH_GR")  # should get rid of site where we didn't have good trait coverage
 
+stabb<-select(stab, Site,UniqueID, Plot_Asynchrony)
+stabb<-filter(stabb, is.na(Plot_Asynchrony)==FALSE)
+stabb<-select(stabb,-Plot_Asynchrony)
 
+all_sync<-select(all, Study, Site, UniqueID, SppN, SppCode6,Year, Spp_Biomass)
 
-[[stop here]]
-spp_site2<-unique(select(all_sync, Study, Site, UniqueID, SppCode6))
+all_syncc<-merge(stabb, all_sync, by.y=c("Site", "UniqueID"))
 
+all_syncc$Year<-as.numeric(all_syncc$Year)
 
+all_syncc$Site<-droplevels(all_syncc$Site)
 
+all_syncc<-arrange(all_syncc,Site, UniqueID, Year)
 
-all_syncc<-merge(spp_site2, all_sync, by.y=c("Study","Site", "UniqueID","SppCode6"),all.x=TRUE)
+all_syncc_NoLanta<-filter(all_syncc,Site!="Lanta_drt")
 
+### fix Lanta_drt
 
-all_Loreau<-synchrony(df=all_sync, time.var="Year", species.var="SppCode6", abundance.var="Spp_Biomass",replicate.var="UniqueID")
+Lantaa<-filter(all_syncc, Site=="Lanta_drt")
 
+des<-unique(select(Lantaa, Study, Site, UniqueID, SppN,SppCode6))
 
-Error in check_multispp(df, species.var, replicate.var) : 
-  One or more replicates consists of only a single species;
-please remove these replicates prior to calculations 
+dess<-des[rep(seq_len(nrow(des)), 3), ]
 
-synch_onerep(df, time.var, species.var, abundance.var, metric = "Loreau")
+dess$Year<-seq(from=1, to=3, by=)
+dess$Year<-rep(1:3, each =384)
 
-check_multispp(alls, species.var="SppCode6", replicate.var="UniqueID")
+comb<-merge(dess, Lantaa, by.y=c("Study","Site","UniqueID","SppN","SppCode6","Year"),all.x=TRUE)
 
-alls2<-filter(alls, Spp_Biomass>0)
+comb$Spp_Biomass<-ifelse(is.na(comb$Spp_Biomass)==TRUE,0,comb$Spp_Biomass)
 
-Spp1 <-summarize(group_by(alls2, Study, Site, UniqueID,Year), realSppN=length_unique(SppCode6))
-Spp1<-filter(Spp1,realSppN>1)
+# add Lanta back
 
-alls3<-merge(Spp1,alls2,by.y=c("Study","Site","UniqueID","Year"))
-
-belg<-filter(alls3, Study=="BigBio")
-
-codyn_Loreau_sync<-synchrony(df=belg, time.var="Year", species.var="SppCode6", abundance.var="Spp_Biomass",replicate.var="UniqueID")
-
-codyn_Gross_sync<-synchrony(df=alls3, time.var="Year", species.var="SppCode6", abundance.var="Spp_Biomass",replicate.var="UniqueID",metric = "Gross")
-
-
-### or 'synchrony'
-
-install.packages("synchrony")
-
-
-require(synchrony)
-
-#######################
-
-c<-merge(fun,jjj,by.y=c("UniqueID"),all.x=TRUE)
-colnames(c)[10]<-"Plot_Biomassxbar"
-colnames(c)[11]<-"Plot_Biomasssd"
-colnames(c)[13]<-"Plot_Asynchrony"
-
-
-write.table(c,"/home/dylan/Dropbox/leipzigPhyTrt/Data/Community/StabRedux_Plot_Level_Stability_24062016.csv",sep=",",row.names=F)
+all_syncc_tog<-rbind.data.frame(all_syncc_NoLanta,comb)
 
 
 
-#####################################
-## community-level              #####
-## asynchrony sensu Gross et al 2013#
-# ave corr of spp biomass with  #####
-## biomass of all other spp, for ####
-## each spp. ########################
-#####################################
+####################
+# Loreau synchrony #
+####################
 
-require(plyr)
-alls<-subset(all,SppN>1)
-alls$PlotBiomass_minus1Spp<-alls$Plot_Biomass-alls$Spp_Biomass
-
-nn<-length_unique(alls$UniqueID)
+nn<-length_unique(all_syncc_tog$Site)
 outt=c();
+
 for(i in 1:nn){
   
-  test=subset(alls, alls$UniqueID==(unique(alls$UniqueID))[i])  
+  test=subset(all_syncc_tog, all_syncc_tog$Site==(unique(all_syncc_tog$Site))[i])  
+
+
+  # output
+cat("progress", i, sep=' ','\n')
+
+loreau<-synchrony(df=test, time.var="Year", species.var="SppCode6", abundance.var="Spp_Biomass",replicate.var="UniqueID")
+colnames(loreau)[2]<-"Loreau_synchrony"
   
-  func1 <- function(test)
-  {
-    return(data.frame(corr=cor(test$Spp_Biomass,test$PlotBiomass_minus1Spp)))
-  }
-  
-  out1<-ddply(test,.(SppCode6),func1)
-  
-  dd<-subset(test,Spp_Biomass>0)
-  realized_spp<- length_unique(dd$SppCode6)
-  r_xbar<-mean(out1$corr,na.rm=T)
-  UniqueID<-unique(test$UniqueID)
-  
-  outt[[i]]<-cbind.data.frame(UniqueID,r_xbar,realized_spp)
-  
+gross<-synchrony(df=test, time.var="Year", species.var="SppCode6", abundance.var="Spp_Biomass",replicate.var="UniqueID",metric="Gross")
+colnames(gross)[2]<-"Gross_synchrony"
+
+togg<-merge(loreau,gross, by.y="UniqueID")
+
+outt[[i]]<-rbind.data.frame(togg)
 }
 
-jjj<-do.call(rbind,outt)  
-jjj <- jjj[!(is.nan(jjj$r_xbar)),]
-colnames(jjj)[2]<-"Community_Asynchrony"
-colnames(jjj)[3]<-"Realized_Spp"
+synzz<-do.call(rbind.data.frame,outt)
+
+
+# merge with big database
+
+stab2<-read.delim("/homes/dc78cahe/Dropbox (iDiv)/Research_projects/leipzigPhyTrt/StabilityII_data/Community_Level/Stab_Stability_FD_PD_CWM_PlotYearAverages_V.csv",sep=",",header=T)
+
+
+stabb_out<-merge(stab2, synzz, by.y=c("UniqueID"),all.x=TRUE)
+
+
+write.table(stabb_out,"/homes/dc78cahe/Dropbox (iDiv)/Research_projects/leipzigPhyTrt/StabilityII_data/Community_Level/Stab_Stability_FD_PD_CWM_PlotYearAverages_VI.csv",sep=",",row.names=F)
+
 
